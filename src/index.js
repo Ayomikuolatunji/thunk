9a281a37cbd4aaeb10d509f9f24909f0efebfc27
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-27 16:45:28 +0100

    persistor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { store } from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'
+import './index.css';
+import { store } from './store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
      <Router>
-      <Provider store={store}>
-        <App />
+      <Provider store={store.store}>
+        <PersistGate loading={null} persistor={store.persistor}>
+          <App />
+        </PersistGate>
       </Provider>
      </Router>
   </React.StrictMode>,
--- src/store.js
+++ src/store.js
@@ -1,12 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createStore,combineReducers,applyMiddleware } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import thunk from 'redux-thunk'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
 import  cartSlice  from "./features/Cart/cartSlice";
 import modalSlice from "./features/modal/modalSlice";
 
-
-
-export const store=configureStore({
-    reducer:{
-       cart:cartSlice,
-       modal:modalSlice
-    }
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const rootReducer= combineReducers({
+    cart:cartSlice,
+    modal:modalSlice
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+let store= createStore(persistedReducer,applyMiddleware(thunk))
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default{
+    store:store,
+    persistor:persistStore(store)
+}
